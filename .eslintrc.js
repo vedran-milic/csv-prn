149a20c77bf13706ef82df81b80e10b52a970824
Vedran Milic <vedran.darkangel@gmail.com>
2022-10-28 20:57:34 +0200

    Add file reader and loosen up typescript
    - add file reader with content preiew
    - loosen up typescript rules to speed up development

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,18 +9,15 @@ module.exports = {
     '@typescript-eslint'
   ],
   extends: [
-    // add more generic rulesets here, such as:
-    // 'eslint:recommended',
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-recommended',
     'prettier'
-    // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
   ],
   rules: {
-    // override/add rules settings here, such as:
     'vue/no-unused-vars': 'error',
-    'no-trailing-spaces': 'error'
+    'no-trailing-spaces': 'error',
+    '@typescript-eslint/ban-ts-comment': 'off'
   }
 }
--- src/components/FileCompare.vue
+++ src/components/FileCompare.vue
@@ -1,6 +1,59 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+const csvContent = ref('');
+const prnContent = ref('');
+function previewFiles(evt: Event) {
+  if (!evt.target) return;
+  // @ts-ignore
+  const [file] = evt.target.files;
+  readFile(file, file.type)
+}
+
+// @ts-ignore
+function readFile(file, type) {
+  const reader = new FileReader();
+
+  reader.addEventListener("load", writeFile.bind(null, reader, type), false);
+
+  if (file) {
+    reader.readAsText(file, 'ISO-8859-3');
+    reader.removeEventListener("load", writeFile.bind(null, reader, type));
+  }
+}
+
+// @ts-ignore
+function writeFile(reader, type) {
+  const content = type === 'text/csv' ? csvContent.value : prnContent.value;
+  // @ts-ignore
+  content.innerText = reader.result;
+}
 </script>
 
 <template>
-  <h1>Hello world</h1>
+  <section class="file-compare">
+    <header class="file-compare-header">
+      <h1>Select files to comapre</h1>
+    </header>
+    <section class="file-compare-body">
+      <section class="file-compare-file-block select-csv-file">
+        <input
+          id="csvFile"
+          type="file"
+          accept=".csv"
+          @change="previewFiles">
+        <div ref="csvContent" class="csv-file-content"></div>
+      </section>
+      <section class="file-compare-file-block select-prn-file">
+        <input
+          id="csvFile"
+          type="file"
+          accept=".prn"
+          @change="previewFiles">
+          <div ref="prnContent" class="prn-file-content"></div>
+      </section>
+    </section>
+    <footer class="file-compare-footer">
+      <button>Compare</button>
+    </footer>
+  </section>
 </template>
